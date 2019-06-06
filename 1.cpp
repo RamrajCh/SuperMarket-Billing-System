1c4e6f670f019afe84e7481705207d9c84a25c1f
PrajwolM <50495631+PrajwolM@users.noreply.github.com>
2019-06-06 18:58:20 +0545

    Trial for modify module

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,26 @@
-//Input module
 #include<iostream>
-using namespace std;
-class{
-private:
-public:};
+
+class ProductId{
+
+	private:
+		int Id;
+		char Name;
+		int price;
+	public :
+		ProductId() {Id=0;}
+
+	};
+
+Modify_Products()
+{
+	cout<< /t <<"YOU ARE GOING TO MODIFY PRODUCTS"<<endl;
+	cout<<"Enter product Name"<<endl;
+	//accesing class varaibales and modifying them;
+	char prod;
+	int nid,nprice;
+	cout <<"Enter product name :/t";
+	cin <<prod;
+	cout<<"enter new id and price";
+    prod.id_set();
+	prod.price_set();
+}
