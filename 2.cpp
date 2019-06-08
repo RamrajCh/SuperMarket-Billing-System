bc2327b191e37fa65195de5792194b70f33c2e46
PrajwolM <pm02433518@student.ku.edu.np>
2019-06-08 11:17:40 +0545

    trial for destuctor

---
+++ 2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+class ProductId{
+
+	private:
+		int Id;
+		char Name;
+		int price;
+	public :
+		ProductId() {Id=0;}
+  ~ProductId();       //this is a destructor
+	};
+void Delete_Products()
+{
+	cout<< /t <<"YOU ARE GOING TO DELETE PRODUCTS"<<endl;
+	cout<<"Enter Product name"<<endl;
+	char *prod;
+	cout <<"Enter product name :/t";
+	cin << *prod;
+	~*prod();
+}
+
